a1588cd6f2ef346d12972c1dc16ba68192bf207e
KrzysztofDolanski <k.dolanski@wp.pl>
2021-08-10 22:57:23 +0200

    Second Correction of task 3 GraphsCounter.java

--- src/main/java/task3/GraphsCounter.java
+++ src/main/java/task3/GraphsCounter.java
@@ -19,8 +19,10 @@ public class GraphsCounter {
         }
         sc.nextLine();
 
+        List<Set<Integer>> list = new ArrayList<>();
+
         int a = 1;
-        List<Set<String>> list = new ArrayList<>();
+        int result = count + 1;
 
         while (count > 0) {
             String s = sc.nextLine();
@@ -29,44 +31,43 @@ public class GraphsCounter {
                 System.err.println("Please, first type number of graphs and than type two integers as an graph. Try again");
                 countGraphs();
             }
+
             while (a > 0) {
-                list.add(Set.of(s1[0], s1[1]));
+                list.add(Set.of(Integer.valueOf(s1[0]), Integer.valueOf(s1[1])));
                 a--;
             }
 
+            boolean flag = true;
             for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).contains(s1[0]) || list.get(i).contains(s1[1])) {
-                    Set<String> set = new HashSet<>();
-                    for (String s2 : list.get(i)) {
-                        set.add(s2);
+                if (list.get(i).contains(Integer.parseInt(s1[0])) || list.get(i).contains(Integer.parseInt(s1[1]))) {
+                    Set<Integer> set1 = new HashSet<>();
+                    for (int j : list.get(i)) {
+                        set1.add(j);
                     }
-                    set.add(s1[0]);
-                    set.add(s1[1]);
-                    list.set(i,set);
+                    set1.add(Integer.parseInt(s1[0]));
+                    set1.add(Integer.parseInt(s1[1]));
+                    list.set(i, set1);
+                    result--;
+                    flag = false;
+                    break;
                 }
             }
+            if (flag)
+                list.add(Set.of(Integer.parseInt(s1[0]), Integer.parseInt(s1[1])));
             count--;
         }
 
 
         for (int i = 0; i < list.size() - 1; i++) {
             for (int i1 = i + 1; i1 < list.size(); i1++) {
-                if (list.get(i) != null || list.get(i1) != null) {
-                    if (Collections.disjoint(list.get(i), list.get(i1))) ;
-                    list.get(i).addAll(list.get(i1));
-                    list.get(i1).clear();
+                for (Integer integer : list.get(i1)) {
+                if (list.get(i).contains(integer))
+                    result--;
+                    break;
                 }
             }
         }
 
-        int result = 0;
-
-
-        for (Set<String> set : list) {
-            if (set!=null){
-                result++;
-            }
-        }
 
         sc.close();
         return result;
