f218abb63607fda225a22821bb49d426bd793854
KrzysztofDolanski <k.dolanski@wp.pl>
2021-08-10 16:57:29 +0200

    Second Correction of task 3 GraphsCounter.java

--- src/main/java/task3/GraphsCounter.java
+++ src/main/java/task3/GraphsCounter.java
@@ -1,16 +1,16 @@
 package task3;
 
-import java.util.HashSet;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class GraphsCounter {
 
+
     public static int countGraphs() {
+
         Scanner sc = new Scanner(System.in);
-        Set<String> set = new HashSet<String>();
+
         int count = 0;
+
         try {
             count = sc.nextInt();
         } catch (InputMismatchException e) {
@@ -18,7 +18,10 @@ public class GraphsCounter {
             countGraphs();
         }
         sc.nextLine();
-        int result = count;
+
+        int a = 1;
+        List<Set<String>> list = new ArrayList<>();
+
         while (count > 0) {
             String s = sc.nextLine();
             String[] s1 = s.split(" ");
@@ -26,16 +29,48 @@ public class GraphsCounter {
                 System.err.println("Please, first type number of graphs and than type two integers as an graph. Try again");
                 countGraphs();
             }
-            if (set.contains(s1[0]) && set.contains(s1[1])) {
-                result -= 2;
-            } else if (set.contains(s1[0]) || set.contains(s1[1])) {
-                result--;
+            while (a > 0) {
+                list.add(Set.of(s1[0], s1[1]));
+                a--;
             }
-            set.add(s1[0]);
-            set.add(s1[1]);
+
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).contains(s1[0]) || list.get(i).contains(s1[1])) {
+                    Set<String> set = new HashSet<>();
+                    for (String s2 : list.get(i)) {
+                        set.add(s2);
+                    }
+                    set.add(s1[0]);
+                    set.add(s1[1]);
+                    list.set(i,set);
+                }
+            }
             count--;
         }
+
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int i1 = i + 1; i1 < list.size(); i1++) {
+                if (list.get(i) != null || list.get(i1) != null) {
+                    if (Collections.disjoint(list.get(i), list.get(i1))) ;
+                    list.get(i).addAll(list.get(i1));
+                    list.get(i1).clear();
+                }
+            }
+        }
+
+        int result = 0;
+
+
+        for (Set<String> set : list) {
+            if (set!=null){
+                result++;
+            }
+        }
+
         sc.close();
         return result;
     }
 }
+
+
